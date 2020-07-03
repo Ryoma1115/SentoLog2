9c9e13dc8e3a92f3816677db7bbaa4326f1dd5b5
vagrant <vagrant@localhost.localdomain>
2020-07-03 00:48:21 +0000

    Add routes for onsenspots, likes, wents, reviews and favorites to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,31 @@
 Rails.application.routes.draw do
 
+  root 'homes#top'
+  get 'about' => 'homes#about'
+
+
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  namespace :users do
+    resources :onsen_spots, only:[:index, :show, :edit, :update, :destroy] do
+      resource :likes, only:[:index, :create, :destroy]
+      resource :wents, only:[:index, :create, :destroy]
+      resources :reviews do
+        resource :favorites, only:[:create, :destroy]
+      end
+    end
+  end
+
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
     registrations: 'admins/registrations'
   }
   
-  devise_for :users, controllers: {
-    sessions:      'users/sessions',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations'
-  }
+
   
 end
