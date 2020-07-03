41c2d8e9f0fac6eafbe21cca238332f0f007d368
vagrant <vagrant@localhost.localdomain>
2020-07-03 09:46:20 +0000

    Add active_for_authentication to model/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    # 退会機能
+    def active_for_authentication?
+      super && (self.is_customer_status == true)
+    end
+
 end
