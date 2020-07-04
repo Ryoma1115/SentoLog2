3c7d08cbfa9c45cfed976009fef16cf1f30f66db
vagrant <vagrant@localhost.localdomain>
2020-07-04 12:12:06 +0000

    Add views for users/onsen_spots/index

--- app/controllers/users/onsen_spots_controller.rb
+++ app/controllers/users/onsen_spots_controller.rb
@@ -2,4 +2,20 @@ class Users::OnsenSpotsController < ApplicationController
     def index
         @onsen_spots = OnsenSpot.all
     end
+
+    private
+        def onsen_spot_params
+            params.require(:onsen_spot).permit(
+                :sensitsu_id,
+                :name,
+                :introduction,
+                :postal_code,
+                :address,
+                :phone_number,
+                :business_hour,
+                :fee,
+                :parking,
+                :image
+            )
+        end
 end
