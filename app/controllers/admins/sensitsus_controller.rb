036869b94bafc3d5448c516c1c982ff500954c6a
vagrant <vagrant@localhost.localdomain>
2020-07-05 00:29:02 +0000

    Add actions for admins/sesitsus_controller

--- app/controllers/admins/sensitsus_controller.rb
+++ app/controllers/admins/sensitsus_controller.rb
@@ -1,2 +1,41 @@
 class Admins::SensitsusController < ApplicationController
+
+    def index
+        @sensitsu = Sensitsu.new
+        @sensitsu = Sensitsu.all
+    end
+
+    def create
+        @sensitsu = Sensitsu(sensitsu_params)
+        if @sensitsu.save
+            redirect_to admins_sensitsus_path
+        else
+            render :edit
+        end
+    end
+
+    def edit
+        @sensitsu = Sensitsu.find(params[:id])
+    end
+
+    def update
+        @sensitsu = Sensitsu.find(params[:id])
+        if @sensitsu.update(sensitsu_params)
+            redirect_to admins_sensitsus_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        sensitsu = Sensitsu.find(params[:id])
+        sensitsu.destroy
+        redirect_to admins_sensitsus_path
+    end
+
+    private
+        def sensitsu_params
+            params.require(:sensitsu).permit(:name)
+        end
+
 end
