35c685a39065b0819685842bb5d41d2210c7ab65
vagrant <vagrant@localhost.localdomain>
2020-07-07 04:43:33 +0000

    Add associations for kounou and oyutype

--- app/models/kounou.rb
+++ app/models/kounou.rb
@@ -1,2 +1,4 @@
 class Kounou < ApplicationRecord
+    has_many :kounou_maps, dependent: :destroy
+    has_many :onsen_spots, through: :kounou_maps
 end
--- app/models/oyutype_map.rb
+++ app/models/oyutype_map.rb
@@ -1,2 +1,4 @@
 class OyutypeMap < ApplicationRecord
+    belongs_to :onsen_spot
+    belongs_to :oyutype
 end
