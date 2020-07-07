53bab97dc1a9ac4691a0b8ceba2ffc8c2ab7cfc1
vagrant <vagrant@localhost.localdomain>
2020-07-07 12:39:11 +0000

    Add actions for controllers/users/kounous and controllers/users/oyutypes

--- app/controllers/users/kounous_controller.rb
+++ app/controllers/users/kounous_controller.rb
@@ -1,5 +1,7 @@
 class Users::KounousController < ApplicationController
   def show
-    
+    @kounou = Kounou.find(params[:id])
+    @kounous = Kounou.all
+    @onsen_spots = OnsenSpot.where(kounou_id: @kounou.id)
   end
 end
--- app/controllers/users/oyutypes_controller.rb
+++ app/controllers/users/oyutypes_controller.rb
@@ -1,4 +1,7 @@
 class Users::OyutypesController < ApplicationController
   def show
+    @oyutype = Oyutype.find(params[:id])
+    @oyutypes = Oyutype.all
+    @onsen_spots = OnsenSpot.where(oyutype_id: @oyutype.id)
   end
 end
