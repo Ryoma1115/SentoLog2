c580ab3b2125cf54ffa7be03344ad029fe93f7b5
vagrant <vagrant@localhost.localdomain>
2020-07-08 14:03:21 +0000

    Add controllers/users/kounous and controllers/users/oyutypes

--- app/controllers/users/kounous_controller.rb
+++ app/controllers/users/kounous_controller.rb
@@ -2,6 +2,7 @@ class Users::KounousController < ApplicationController
   def show
     @kounou = Kounou.find(params[:id])
     @kounous = Kounou.all
-    @onsen_spots = OnsenSpot.where(kounou_id: @kounou.id)
+    @kounou_maps = KounouMap.where(kounou_id: @kounou.id).pluck(:onsen_spot_id)
+    @onsen_spots = OnsenSpot.find(@kounou_maps)
   end
 end
