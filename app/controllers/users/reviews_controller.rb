e701269644cc5ce8c20266acfcd79008aea5e7bf
vagrant <vagrant@localhost.localdomain>
2020-07-09 07:46:51 +0000

    Add controllers/users/reviews for create

--- app/controllers/users/reviews_controller.rb
+++ app/controllers/users/reviews_controller.rb
@@ -4,10 +4,24 @@ class Users::ReviewsController < ApplicationController
     end
 
     def create
-
+        @onsen_spot = OnsenSpot.find(params[:onsen_spot_id])
+        @review = @onsen_spot.review.new
+        @review.user_id = current_user.id
+        if @review.save
+            flash[:success] = "口コミ投稿されました。"
+            redirect_to users_onsen_spot(@onsen_spot)
+        else
+            @reviews = Reviews.where(onsen_spot_id: @onsen_spot.id)
+            render '/onsen_spots/show'
+        end
     end
 
     def destroy
-        
+
+    end
+
+    private
+    def review_params
+        params.require(:review).permit(:title, :comment)
     end
 end
