aa1b906b5a3ea28e8f09675e4d2baab82465746b
vagrant <vagrant@localhost.localdomain>
2020-07-11 06:24:40 +0000

    Add actions for controllers/users/wents.

--- app/controllers/users/wents_controller.rb
+++ app/controllers/users/wents_controller.rb
@@ -1,2 +1,13 @@
 class Users::WentsController < ApplicationController
+
+    def create
+        went = current_user.wents.build(onsen_spot_id: params[:onsen_spot_id])
+        went.save
+    end
+
+    def destroy
+        went = Went.find_by(onsen_spot_id: params[:onsen_spot_id], user_id: current_user.id)
+        went.destroy
+    end
+
 end
