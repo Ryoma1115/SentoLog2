05f7189b3679ff94c443d1ee822440c48e487d78
vagrant <vagrant@localhost.localdomain>
2020-07-11 12:24:08 +0000

    Add views/users/favorites/_favorite.

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,9 +6,12 @@ class Review < ApplicationRecord
     has_many :favorites
 
     validates :comment, presence: true
-
     validates :rate, numericality: {
         less_than_or_equal_to: 5,
         greater_than_or_equal_to: 1
       }, presence: true
+
+    def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+    end
 end
