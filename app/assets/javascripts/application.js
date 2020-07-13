b2517b297d7f6a6a13486285a9df43aebc1b6b30
vagrant <vagrant@localhost.localdomain>
2020-07-13 13:25:20 +0000

    Modified views/layouts/_header.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,16 +17,6 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(function(){
-  $('.js-modal-open').on('click',function(){
-      $('.js-modal').fadeIn();
-      return false;
-  });
-  $('.js-modal-close').on('click',function(){
-      $('.js-modal').fadeOut();
-      return false;
-  });
-});
 
 // 星評価機能
 (function($) {
