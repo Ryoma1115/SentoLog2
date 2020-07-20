c2f4269269d34b1a329df2ea1b4926441944c9dc
vagrant <vagrant@localhost.localdomain>
2020-07-20 13:09:30 +0000

    Add rspec for models/kounou_map.

--- spec/models/kounou_map_spec.rb
+++ spec/models/kounou_map_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe KounouMap, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'アソシエーションのテスト' do
+    context 'OnsenSpotモデルとの関係' do
+      it 'N:1となっている' do
+        expect(KounouMap.reflect_on_association(:onsen_spot).macro).to eq :belongs_to
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Kounouモデルとの関係' do
+      it 'N:1となっている' do
+        expect(KounouMap.reflect_on_association(:kounou).macro).to eq :belongs_to
+      end
+    end
+  end
+
 end
