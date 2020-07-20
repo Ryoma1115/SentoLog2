1d35e0f22af9438ad035b0458858ed369c3316bf
vagrant <vagrant@localhost.localdomain>
2020-07-20 12:55:12 +0000

    Add rspec for models/favorites.

---
+++ spec/factories/kounous.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :kounou do
+    
+  end
+end
---
+++ spec/models/kounou_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Kounou, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
