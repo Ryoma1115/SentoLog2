14edd5e61a9120d3ce6c64ec719e9119674930aa
vagrant <vagrant@localhost.localdomain>
2020-07-21 13:55:29 +0000

    Add search form for views/users/onsen_spots/index.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
     def set_search
         #以下は検索に使うときの記述（変数やモデル名は変更してもいいですが
         #".ransack(params[:q])"はそのままで使用します。）
+        # @q = params[:q]
         @search = OnsenSpot.ransack(params[:q])
-        
+        # @onsen_spots = OnsenSpot.search(name_cont: @q).result
         #以下は検索したものを表示する時に使う記述(一番シンプルで基本の形です)
         @onsen_spots = @search.result
     end
