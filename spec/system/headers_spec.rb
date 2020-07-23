b70a2305772b320ad8a88c6792391d74694191de
vagrant <vagrant@localhost.localdomain>
2020-07-23 15:16:11 +0000

    Add spec/system/headers.

--- spec/system/headers_spec.rb
+++ spec/system/headers_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "Headers", type: :system do
 
   describe 'ヘッダーのテスト' do
     describe 'ログインしていない場合' do
+      let(:sensitsu) { create(:sensitsu) }
       before do
         visit root_path
       end
@@ -15,8 +16,38 @@ RSpec.describe "Headers", type: :system do
         it 'ロゴが表示されているか' do
           expect(page).to have_link '', href: root_path
         end
+        it 'モーダルの泉質が表示されている' do
+          expect(page).to have_content('泉質')
+        end
+        it 'ドロップダウンメニュー の都道府県が表示されている' do
+          expect(page).to have_content('泉質')
+        end
         it '温泉地を登録するリンクが表示されている' do
           expect(page).to have_content('温泉地を登録する')
+        end
+        it '温泉一覧リンクが表示されている' do
+          expect(page).to have_content('温泉一覧')
+        end
+        it '新規登録リンクが表示されている' do
+          expect(page).to have_content('新規登録')
+        end
+        it 'ログインリンクが表示されている' do
+          expect(page).to have_content('ログイン')
+        end
+        it 'Aboutリンクが表示されている' do
+          expect(page).to have_content('About')
+        end
+      end
+
+      context 'ヘッダーのリンクを確認' do
+        it 'モーダルの泉質リンクから泉質showに移動' do
+          # expect(page).to have_link '', href: users_sensitsu_path(sensitsu.id)
+        end
+        it 'モーダルの効能リンクから効能showに移動' do
+          # expect(page).to have_link '', href: users_kounou_path(kounou.id)
+        end
+        it 'モーダルのお湯タイプリンクからお湯タイプshowに移動' do
+          # expect(page).to have_link '', href: users_oyutype_path(oyutype.id)
         end
       end
     end
